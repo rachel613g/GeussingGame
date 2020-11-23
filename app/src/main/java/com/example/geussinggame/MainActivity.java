2bb5185fc7bc305f684e65d58448bb3cf6c56dd6
rachel613g <rachelgutmann613@gmail.com>
2020-11-22 21:49:51 -0500

    added MainActivity class provided. generated icon launcher files using AndroidStudio.

--- app/src/main/java/com/example/geussinggame/MainActivity.java
+++ app/src/main/java/com/example/geussinggame/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.geussinggame;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,24 +14,40 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GuessingGame mGame;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("GAME", mGame.getJSONStringFromThis());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupToolbar();
+        setupFAB();
+        mGame = savedInstanceState != null
+                ? GuessingGame.getGuessingGameObjectFromJSONString(
+                savedInstanceState.getString("GAME"))
+                : new GuessingGame();
+    }
+
+    private void setupToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
+    private void setupFAB() {
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> mGame.startNewGame());
     }
 
     @Override
@@ -53,4 +71,16 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void pick123(View view) {
+        Button currentButton = (Button) view;
+        String currentButtonText = currentButton.getText().toString();
+        String resultsText = currentButtonText.equals(String.valueOf(mGame.getWinningNumber()))
+                ? String.format(Locale.US, "Yes! %s is the right number.", currentButtonText)
+                : String.format(Locale.US, "Sorry. %s is not the right number.", currentButtonText);
+
+        Snackbar.make(view, resultsText, Snackbar.LENGTH_SHORT).show();
+    }
 }
+
+
